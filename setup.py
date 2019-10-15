263668fb09c11b379df06d34e486fd1f34232c3a
Greg Schwimer <schwim@bitrail.com>
2019-10-15 14:20:31 -0700

    cli operational

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'numpy>=1.17', 'pandas>=0.25', 'astropy>=3.2' ]
 
 setup_requirements = [ ]
 
@@ -20,16 +20,12 @@ test_requirements = [ ]
 setup(
     author="Greg Schwimer",
     author_email='schwim@bitrail.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
